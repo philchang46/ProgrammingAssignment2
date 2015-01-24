5b830a61491b4545467d9b8e1259fc4b06cb2be6
philchang46 <philchang46@users.noreply.github.com>
2015-01-24 01:22:39 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,4 @@
-## https://github.com/philchang46/ProgrammingAssignment2.git
-## 1st commit SHA-1 hash identifier: 
 ## R Programming Assignment 2: Lexical Scoping
-
 ## The makeCacheMatrix function creates a special "matrix", which
 ## which is a list containing a function to "set and get" the value
 ## of the matrix, and its inverse.
@@ -15,15 +12,12 @@ makeCacheMatrix <- function(x = matrix()) {
         setcache <- function(cache) c <<- cache
         getcache <- function() c
         list(set = set, get = get, setcache = setcache, getcache = getcache)
-  
 }
-
 ## The cacheSolve function calculates the inverse of the special "matrix"
 ## which created with the makeCacheMatrix function. First, it checks if 
 ## the inverse value has already been calculated. If it has, the inverse 
 ## value is obtained. Otherwise, it calculates the inverse matrix and 
 ## sets the value of the inverse in the cache via the setinverse function.
-
 cacheSolve <- function(x, ...) {
     ## Return the value of matrix that is the inverse of x
     c <- x$getcache()
